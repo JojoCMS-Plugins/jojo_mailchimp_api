8228b6a11baa30040844d2e3911a0a3ed68a8520
Harvey Kane <harvey@ragepank.com>
2011-06-16 03:59:29 +0000

    Add merge vars to the API call

--- jojo_mailchimp_api.php
+++ jojo_mailchimp_api.php
@@ -14,7 +14,7 @@
 
 class Jojo_Plugin_Jojo_Mailchimp_API extends Jojo_Plugin
 {
-    function subscribe($email, $list_id=false)
+    function subscribe($email, $list_id=false, $merge_vars=NULL)
     {
         if (!$list_id) $list_id = Jojo::getOption('mailchimp_cart_list_id', false);
         if (empty($list_id)) return false;
@@ -29,7 +29,7 @@ class Jojo_Plugin_Jojo_Mailchimp_API extends Jojo_Plugin
         
     	$api = new MCAPI($key);  
     	/* note the double-opt-in is disabled here */
-    	return $api->listSubscribe($list_id, $email, NULL, 'html', false); //args = ($id, $email_address, $merge_vars=NULL, $email_type='html', $double_optin=true, $update_existing=false, $replace_interests=true, $send_welcome=false)
+    	return $api->listSubscribe($list_id, $email, $merge_vars, 'html', false); //args = ($id, $email_address, $merge_vars=NULL, $email_type='html', $double_optin=true, $update_existing=false, $replace_interests=true, $send_welcome=false)
     }
     
     function jojo_cart_success($cart)
@@ -42,7 +42,7 @@ class Jojo_Plugin_Jojo_Mailchimp_API extends Jojo_Plugin
         if (empty($email) || !Jojo::checkEmailFormat($email)) return false; //one would hope email address is valid at this point
         
         /* attempt to subscribe */
-        return self::subscribe($email);        
+        return self::subscribe($email, $list_id, array('FNAME' => $cart->fields['billing_firstname'], 'LNAME' => $cart->fields['billing_firstname']));        
     }
     
 }
